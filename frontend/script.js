e1ea8b65817425d6b3830ec877fb88ac5d5279c6
Saul Tellez <saultellezmastranzo@gmail.com>
2025-12-09 22:48:28 -0600

    New endpoint

--- frontend/script.js
+++ frontend/script.js
@@ -50,6 +50,66 @@ function displayTodos(todos) {
      </div>
    `).join('');
 }
+function enableEdit(id, currenTitle){
+    // Funcion para habilitar la edicion (al hacer doble clic o con el botón Editar)
+    const item = document.getElementById(`todo-${id}`);
+    const textSpan = document.getElementById(`text-${id}`);
+    // Crear un input field para la edicion
+    const inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.value = currenTitle;
+    inputField.className = 'edit-input';
+
+    // Crear el botón de guardar 
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Guardar';
+    saveButton.className = 'save-btn';
+    saveButton.onclick = () => saveTodo(id , inputField.value);
+
+    // Reemplazar el span con el input y añadir el botón de guardar
+    item.replaceChild(inputField, textSpan);
+    item.querySelector('.actions').innerHTML = ''; //Limpiar acciones existentes
+    item.querySelector('.actions').appendChild(saveButton);
+
+    inputField.focus();
+    //  Permite guardar con Enter
+    inputField.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            saveTodo(id , inputField.value);
+
+        }
+    });
+}
+// Funcioón para guardar los cambios en el backend (Llama al endpoint Put)
+async function saveTodo(id, newTitle) {
+   const title = newTitle.trim();
+   if (!title) {
+    alert('El título no puede estar vacío');
+    return
+    }
+
+    try {
+        const response = await fetch (`${API_URL}/todos/${id}`,{
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({title}) //Usa el esquema TodoCreate
+    });
+    if (response.ok) {
+        loadTodos();
+
+     }else{
+        alert('No se pudo guardar la tarea');
+     }
+
+    } catch (error) {
+        console.error('Error al actualizar tarea:', error);
+        alert('Error al actualizar la tarea');
+           }
+        }
+ 
+
 
 // Función para crear una nueva tarea
          async function createTodo() {
